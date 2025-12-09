0c4109d413762a0d09028e8ffb456c416a4c95f1
BossMac <bossmac@BossMacs-MacBook-Air.local>
2025-12-09 23:09:05 +0000

    update version

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   // 'base: ./' ensures the app works if hosted at root (dmicdt.github.io) 
   // or a subdirectory (dmicdt.github.io/planner)
-  base: './', 
+  base: '/', 
   build: {
     outDir: 'dist',
   }
